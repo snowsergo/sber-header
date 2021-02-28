569809c929d8f79f21a50eef518e43ee5b4016d3
Sergey Stupnikov <sergey.stupnikov.59@yandex.ru>
2021-02-28 23:50:01 +0500

    readme changed

--- babel.config.js
+++ babel.config.js
@@ -2,15 +2,15 @@ const presets = [
   [
     "@babel/preset-env",
     {
-      targets: { 
+      targets: {
             edge: "15",
             ie: "11",
             firefox: "50",
             chrome: "64",
             safari: "11.1",
       },
-      useBuiltIns: "usage", // эта настройка babel-polyfill, если стоит значение usage, то будут подставлятся полифилы для версий браузеров которые указали ниже.
-      corejs: "3.4.1" // явно проставить версию corejs
+      useBuiltIns: "usage",
+      corejs: "3.4.1"
     }
   ],
 ];
